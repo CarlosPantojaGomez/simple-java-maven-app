296eaf25716c71582b6680171a41c164cf23ed4a
Carlos Pantoja Gómez <CarlosPantoja@protonmail.com>
2020-02-26 12:56:45 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello cosmuuumit";
+    private final String message = "Hello cosmdfuuumit";
 
     public App() {}
 
